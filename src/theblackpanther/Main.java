7d602ad374a7155fdb1168aa263baa2ab58fcd54
dobre <dobrev8444@gmail.com>
2018-07-04 14:46:33 +0300

    class Main added into package

--- src/theblackpanther/Main.java
+++ src/theblackpanther/Main.java
@@ -1,21 +1,8 @@
 package theblackpanther;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        CalendarApp calendar = new CalendarApp();
 
-        System.out.println(calendar);
-
-        Scanner input = new Scanner(System.in);
-        int ourInput = input.nextInt();
-
-        while(ourInput != 0){
-            // type 0 for termination
-            switch (ourInput){
-                //cases..
-            }
-        }
     }
+
 }
