2c6f0f4b795fb0e89e5ea4bbd4c862096a8d4efb
TeodoraFilipova <you@example.com>
2018-07-04 14:01:01 +0300

    updated/filled class Meeting.

--- src/theblackpanther/Meeting.java
+++ src/theblackpanther/Meeting.java
@@ -3,12 +3,19 @@ package theblackpanther;
 import java.util.ArrayList;
 
 public class Meeting extends Event implements Editable, Deletable {
-    String place;
-    ArrayList<String> participants;
+    private String place;
+    private ArrayList<String> participants;
+
+    public Meeting(String name, String date, String startHour, String endHour, String description, String place) {
+        super(name, date, startHour, endHour, description);
+        setPlace(place);
+        setParticipants();
+    }
 
     @Override
     public void displayEvent() {
-
+        System.out.printf("Event Name: %s\nEvent Date: %s\nEvent Start Time: %s\nEvent End Time: %s\nEvent Description: %s\nEvent Location: %s\n" +
+                "Event Participants: %s\n", getName(), getDate(), getStartHour(), getEndHour(), getDescription(), getPlace(), getParticipants());
     }
 
     /*@Override
@@ -17,16 +24,36 @@ public class Meeting extends Event implements Editable, Deletable {
     }*/
 
     void addParticipant(String participant){
-
+        participants.add(participant);
     }
 
     @Override
     public void edit(String name, String date, String startHour, String endHour, String description) {
-
+        super.setName(name);
+        super.setDate(date);
+        super.setStartHour(startHour);
+        super.setEndHour(endHour);
+        super.setDescription(description);
     }
 
     @Override
     public void delete() {
 
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public ArrayList<String> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants() {
+        this.participants = new ArrayList<>();
     }
 }
