4ff16fe49a536e8e98f8b02a664ec2ff0b52d296
Konstantin Merdzhanov <kmerdjanov@gmail.com>
2018-07-05 13:19:07 +0300

    editEvent method added

---
+++ src/theblackpanther/DateNotInCalendarException.java
@@ -0,0 +1,32 @@
+package theblackpanther;
+
+public class DateNotInCalendarException extends IllegalArgumentException{
+    private static final long serialVersionUID = 7718828512143293558L;
+
+    private final WrongDateElementType code;
+
+    public DateNotInCalendarException(WrongDateElementType code) {
+        super();
+        this.code = code;
+    }
+    public DateNotInCalendarException(String message, Throwable cause, WrongDateElementType code) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    public DateNotInCalendarException(String message, WrongDateElementType code) {
+        super(message);
+        this.code = code;
+    }
+
+    public DateNotInCalendarException(Throwable cause, WrongDateElementType code) {
+        super(cause);
+        this.code = code;
+    }
+
+    public WrongDateElementType getCode() {
+        return this.code;
+    }
+
+}
+
---
+++ src/theblackpanther/WrongDateElementType.java
@@ -0,0 +1,5 @@
+package theblackpanther;
+
+public enum WrongDateElementType {
+    WRONG_YEAR, WRONG_MONTH, WRONG_DATE
+}
