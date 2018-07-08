13333ba7c7decb79436b6e42d6647608f8914f6e
dobre <dobrev8444@gmail.com>
2018-07-08 16:46:23 +0300

    update class Month (access modifiers changed, rearrangement made, methods optimized)

--- src/theblackpanther/Month.java
+++ src/theblackpanther/Month.java
@@ -8,30 +8,21 @@ public class Month {
     private MonthName name;
     private ArrayList<Day> days;
     private ArrayList<Day> eventDays;
-    private int numberOfDays;
 
 
-    public Month(MonthName name, int numberOfDays) {
+    Month(MonthName name) {
         setName(name);
         setDays(new ArrayList<Day>());
         setEventDays(new ArrayList<Day>());
-        setNumberOfDays(numberOfDays);
     }
 
-    public boolean isThereEvent(Day currentDay){
-        if(currentDay.getEventListForDay().size() != 0){
-            return true;
-        }
-        return false;
-    }
-
-    public void addDayIntoMonth(Day newDay){
+    void addDayIntoMonth(Day newDay){
         days.add(newDay);
     }
 
-    public void displayAllDays(){
+    void displayAllDays(){
 
-        long daysSize = days.stream().count();
+        int daysSize = days.size();
 
         days.stream()
                 .limit(7)
@@ -56,41 +47,30 @@ public class Month {
                 .forEach(day -> System.out.printf("%3s", day.getDate() + " "));
         System.out.println();
 
-        days.subList(28, (int)daysSize)
+        days.subList(28, daysSize)
                 .stream()
                 .limit(7)
                 .forEach(day -> System.out.printf("%3s", day.getDate() + " "));
         System.out.println();
 
-//        int counter = 0;
-//        Day currentDay;
-//        for(int i = 0; i < days.size(); i++){
-//            currentDay = days.get(i);
-//            if(isThereEvent(currentDay)){
-//                eventDays.add(currentDay);
-//            }
-//            if(counter < 7) {
-//                System.out.print(days.get(i) + " ");
-//                counter++;
-//            }else{
-//                System.out.println();
-//                counter = 0;
-//            }
-//        }
     }
 
-    public void displayAllDaysWithEvents(){
+    private boolean isThereEvent(Day currentDay){
+        return currentDay.getEventListForDay().size() != 0;
+    }
+
+    void displayAllDaysWithEvents(){
         List<Day> filtered = days.stream().filter(this::isThereEvent).collect(Collectors.toList());
         eventDays.addAll(filtered);
 
-        for(int currentDay = 0; currentDay < eventDays.size(); currentDay++) {
-            System.out.println(eventDays.get(currentDay).getDayOfWeek() + "/DAY: " + eventDays.get(currentDay).getDate() + ":");
-            for(int currentEvent = 0; currentEvent < eventDays.get(currentDay).getEventListForDay().size(); currentEvent++){
+        for (Day eventDay : eventDays) {
+            System.out.println(eventDay.getDayOfWeek() + "/DAY: " + eventDay.getDate() + ":");
+            for (int currentEvent = 0; currentEvent < eventDay.getEventListForDay().size(); currentEvent++) {
                 System.out.println("Description of the event :\n" +
-                        eventDays.get(currentDay)
-                        .getEventListForDay()
-                        .get(currentEvent)
-                        .getDescription());
+                        eventDay
+                                .getEventListForDay()
+                                .get(currentEvent)
+                                .getDescription());
             }
         }
     }
@@ -103,31 +83,15 @@ public class Month {
         this.name = name;
     }
 
-    public ArrayList<Day> getDays() {
-        return days;
-    }
-
-    public void setDays(ArrayList<Day> days) {
+    private void setDays(ArrayList<Day> days) {
         this.days = days;
     }
 
-    public ArrayList<Day> getEventDays() {
-        return eventDays;
-    }
-
-    public void setEventDays(ArrayList<Day> eventDays) {
+    private void setEventDays(ArrayList<Day> eventDays) {
         this.eventDays = eventDays;
     }
 
-    public int getNumberOfDays() {
-        return numberOfDays;
-    }
-
-    public void setNumberOfDays(int numberOfDays) {
-        this.numberOfDays = numberOfDays;
-    }
-
-    public Day returnDay(String dayNumber) {
+    Day returnDay(String dayNumber) {
         for (Day dayInstance : this.days) {
             if (dayInstance.getDate().equals(dayNumber)) {
                 return dayInstance;
