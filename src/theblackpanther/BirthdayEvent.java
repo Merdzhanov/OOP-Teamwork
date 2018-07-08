5620ffa29b4e549a9dc22706dd3e817cd6a49560
dobre <dobrev8444@gmail.com>
2018-07-08 16:30:27 +0300

    update class Birthday (access modifiers changed, rearrangement made)

--- src/theblackpanther/BirthdayEvent.java
+++ src/theblackpanther/BirthdayEvent.java
@@ -6,8 +6,8 @@ public class BirthdayEvent extends Event {
     private String nameOfBirthdayPerson;
     private String gift;
 
-    public BirthdayEvent(String name, String date, String startHour, String endHour,
-                         String description, String place, String nameOfBirthdayPerson, String gift) {
+    BirthdayEvent(String name, String date, String startHour, String endHour,
+                  String description, String place, String nameOfBirthdayPerson, String gift) {
         super(name, date, startHour, endHour, description);
         setPlace(place);
         setNameOfBirthdayPerson(nameOfBirthdayPerson);
@@ -21,24 +21,24 @@ public class BirthdayEvent extends Event {
                          "gift:                 " + getGift() + "\n");
     }
 
-    public String getPlace() {
-        return place;
-    }
-
-    public void setPlace(String place) {
-        this.place = place;
-    }
-
     private String getNameOfBirthdayPerson() {
         return nameOfBirthdayPerson;
     }
 
-    private void setNameOfBirthdayPerson(String nameOfBirthdayPerson) {
-        this.nameOfBirthdayPerson = nameOfBirthdayPerson;
+    private String getPlace() {
+        return place;
     }
 
     private String getGift() {
         return gift;
+    }
+
+    private void setPlace(String place) {
+        this.place = place;
+    }
+
+    private void setNameOfBirthdayPerson(String nameOfBirthdayPerson) {
+        this.nameOfBirthdayPerson = nameOfBirthdayPerson;
     }
 
     private void setGift(String gift) {
