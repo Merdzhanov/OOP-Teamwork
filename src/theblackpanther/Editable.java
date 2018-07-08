dd3625195fa106a8e8a90d469053a4a8e001e905
dobre <dobrev8444@gmail.com>
2018-07-08 16:35:59 +0300

    update interface Editable (comment removed)

--- src/theblackpanther/Editable.java
+++ src/theblackpanther/Editable.java
@@ -2,5 +2,4 @@ package theblackpanther;
 
 public interface Editable {
     void edit(String name, String date, String startHour, String endHour, String description);
-    // Add Override for each subclass in Event
 }
